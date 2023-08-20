42463828e92e0e1cc78dc95a8f21eb642cb7d6f5
Andrei-David Nan <nandavid190@gmail.com>
2023-08-20 18:22:07 +0300

    v0.3 - Added Delete & Load methods and comments;

--- todo.go
+++ todo.go
@@ -1,21 +1,27 @@
+// Package todo provides functionality for a simple CLI todo application.
 package todo
 
 import (
+	"encoding/json"
 	"errors"
+	"os"
 	"time"
 )
 
-type item struct {
+// Item represents a single todo item.
+type Item struct {
 	Task        string
 	Done        bool
 	CreatedAt   time.Time
 	CompletedAt time.Time
 }
 
-type Todos []item
+// Todos is a collection of todo items.
+type Todos []Item
 
+// Add adds a new task to the todo list.
 func (t *Todos) Add(task string) {
-	todo := item{
+	todo := Item{
 		Task:        task,
 		Done:        false,
 		CreatedAt:   time.Now(),
@@ -25,14 +31,48 @@ func (t *Todos) Add(task string) {
 	*t = append(*t, todo)
 }
 
+// Complete marks a task as completed by index.
 func (t *Todos) Complete(index int) error {
-	lst := *t
-	if index <= 0 || index > len(lst) {
-		return errors.New("Invalid index!")
+	if index < 1 || index > len(*t) {
+		return errors.New("invalid index")
 	}
 
-	lst[index-1].CompletedAt = time.Now()
-	lst[index-1].Done = true
+	item := &(*t)[index-1]
+	item.CompletedAt = time.Now()
+	item.Done = true
+
+	return nil
+}
+
+// Delete removes a task by index.
+func (t *Todos) Delete(index int) error {
+	if index < 1 || index > len(*t) {
+		return errors.New("invalid index")
+	}
+
+	*t = append((*t)[:index-1], (*t)[index:]...)
+
+	return nil
+}
+
+// Load reads and deserializes todo items from a file.
+func (t *Todos) Load(filename string) error {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+
+	if len(file) == 0 {
+		return nil
+	}
+
+	err = json.Unmarshal(file, t)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
