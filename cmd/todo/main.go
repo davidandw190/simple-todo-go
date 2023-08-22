eb397fbd10d16e0c0defd9bd94374caa9d68fc85
Andrei-David Nan <nandavid190@gmail.com>
2023-08-22 22:11:21 +0300

    v0.5 - Added logic for Add in main.go;

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	todo "github.com/davidandw190/simple-todo-go"
+)
+
+const (
+	todoFile = ".todo.json"
+)
 
 func main() {
-	fmt.Println("Lets go")
+
+	add := flag.Bool("add", false, "-> add a new todo item")
+
+	flag.Parse()
+
+	todos := &todo.Todos{}
+
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+		todos.Add("Sample todo")
+		err := todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	default:
+		fmt.Fprint(os.Stdout, "invalid command")
+		os.Exit(0)
+	}
 }
