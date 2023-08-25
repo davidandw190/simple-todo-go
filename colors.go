f5373890a96bd6d1fb7e2c10038a7f37fcb65816
Andrei-David Nan <nandavid190@gmail.com>
2023-08-25 17:15:43 +0300

    v0.12 - Small cleanup, enhanced error messages;

--- colors.go
+++ colors.go
@@ -1,6 +1,10 @@
 package todo
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 const (
 	ColorDefault = "\x1b[39m"
@@ -11,18 +15,39 @@ const (
 	ColorGrey  = "\x1b[90m"
 )
 
-func red(s string) string {
+func Red(s string) string {
 	return fmt.Sprintf("%s%s%s", ColorRed, s, ColorDefault)
 }
 
-func green(s string) string {
+func Green(s string) string {
 	return fmt.Sprintf("%s%s%s", ColorGreen, s, ColorDefault)
 }
 
-func blue(s string) string {
+func Blue(s string) string {
 	return fmt.Sprintf("%s%s%s", ColorBlue, s, ColorDefault)
 }
 
-func grey(s string) string {
+func Grey(s string) string {
 	return fmt.Sprintf("%s%s%s", ColorGrey, s, ColorDefault)
+}
+
+func PrintRed(w io.Writer, s string) {
+	fmt.Fprintf(w, "%s%s%s", ColorRed, s, ColorDefault)
+}
+
+func PrintGreen(w io.Writer, s string) {
+	fmt.Fprintf(w, "%s%s%s", ColorGreen, s, ColorDefault)
+}
+
+func PrintBlue(w io.Writer, s string) {
+	fmt.Fprintf(w, "%s%s%s", ColorBlue, s, ColorDefault)
+}
+
+func PrintGrey(w io.Writer, s string) {
+	fmt.Fprintf(w, "%s%s%s", ColorGrey, s, ColorDefault)
+}
+
+// PrintRedStderr prints red text to os.Stderr
+func PrintRedStderr(s string) {
+	PrintRed(os.Stderr, s)
 }
